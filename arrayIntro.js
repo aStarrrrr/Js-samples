04521ba1bc2303b8cc2684b5f51aac9f734ca7c6
Abhinand Shaji <abhinandshaji369@gmail.com>
2024-12-26 20:39:29 +0530

    Array intro

---
+++ arrayIntro.js
@@ -0,0 +1,17 @@
+let teamMem = ['Kaattu','Gopu','Kannan']
+
+console.log(teamMem)
+
+console.log('Size of the team is '+teamMem.length)
+
+teamMem[3] = 'Achu'
+
+console.log(teamMem)
+
+console.log('Size of the team become '+teamMem.length)
+
+console.log(teamMem[3])
+
+teamMem[4] = 40
+
+console.log(teamMem)
