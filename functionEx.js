f096c4b7b8ec33de7e02f09090a59a287b2a94cc
Abhinand Shaji <abhinandshaji369@gmail.com>
2024-12-26 20:51:46 +0530

    Array intro

---
+++ functionEx.js
@@ -0,0 +1,12 @@
+function f1() {
+    console.log('Hellow world!')
+}
+f1()
+function f2(name) {
+    console.log('My name is ' + name)
+}
+f2('Abhinand Shaji')
+function f3(name,lname) {
+    console.log('My name is ' + name + ' ' + lname)
+}
+f3('Karthika','R')
