097a629a0acdd261ff05a1e35299acecafb08f82
Abhinand Shaji <abhinandshaji369@gmail.com>
2024-12-26 17:48:32 +0530

    Premitive types

---
+++ primitivetypes.js
@@ -0,0 +1,7 @@
+let userName = 'aStar'; //String Literal
+let age = 20; //Number Literal
+let isApproved = true; //Booloean
+let backgroundColor = undefined; //Undefined
+let selectedColor = null; //Null
+
+console.log('My name is '+userName)
--- sample.js
+++ sample.js
@@ -2,6 +2,6 @@ var name='Abbhinand Shaji'
 var n=40
 console.log('Iam '+name +n)
 alert('Ok')
-const interestRate = 3
+var interestRate = 3
 interestRate = 1
 console.log(interestRate + ' is the interest rate.')
